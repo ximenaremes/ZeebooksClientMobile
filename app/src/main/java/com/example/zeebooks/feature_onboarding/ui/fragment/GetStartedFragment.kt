217ee75c04606a1a9c59c74f0f9e966d698c1667
Ximena Remes <ioana.remes@btrl.ro>
2023-05-30 22:06:09 +0300

    bottom nav

--- app/src/main/java/com/example/zeebooks/feature_onboarding/ui/fragment/GetStartedFragment.kt
+++ app/src/main/java/com/example/zeebooks/feature_onboarding/ui/fragment/GetStartedFragment.kt
@@ -69,7 +69,7 @@ class GetStartedFragment : BaseFragment<FragmentGetStartedBinding>() {
        }
 
         binding.textSkip.setOnClickListener {
-            findNavController().navigate(R.id.action_getStartedFragment_to_loginFragment)
+            findNavController().navigate(R.id.action_getStartedFragment_to_mainActivity)
         }
 
 //        if (position < pagerAdapter.itemCount){
