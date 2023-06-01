8df131c5c67e16a2a71eda09a195478ba565bdae
Ximena Remeș <78303359+ximenaremes@users.noreply.github.com>
2023-06-01 09:18:55 +0300

    added bottom navigation (#1)
    * bottom nav
    
    * added bottom navigation
    
    ---------
    
    Co-authored-by: Ximena Remes <ioana.remes@btrl.ro>

--- app/src/main/java/com/example/zeebooks/feature_dashboard/ui/activity/DashboardActivity.kt
+++ app/src/main/java/com/example/zeebooks/feature_dashboard/ui/activity/DashboardActivity.kt
@@ -1,64 +1,38 @@
 package com.example.zeebooks.feature_dashboard.ui.activity
 
 import android.os.Bundle
-import android.view.MenuItem
 import android.view.View
-import androidx.core.view.forEach
-import androidx.databinding.adapters.AdapterViewBindingAdapter.setOnItemSelectedListener
-import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.zeebooks.R
-import com.example.zeebooks.commons.ui.activity.BaseActivity
 import com.example.zeebooks.databinding.ActivityDashboardBinding
-import com.example.zeebooks.feature_dashboard.ui.fragment.CategoriesFragment
-import com.example.zeebooks.feature_dashboard.ui.fragment.ExploreFragment
-import com.example.zeebooks.feature_dashboard.ui.fragment.ReviewFragment
-import com.example.zeebooks.feature_profile.ui.fragment.ProfileFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 
-class DashboardActivity: BaseActivity() {
-
-    override val contentResId = R.layout.activity_dashboard
+class DashboardActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDashboardBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setupBottomNavigation()
-
-    }
-
-    private fun setupBottomNavigation() {
-
         binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navController = findNavController(R.id.nav_dashboard_items)
 
-        binding.bottomNavigation.apply{
-            setOnItemSelectedListener { item ->
-                when (item.itemId) {
-                    R.id.exploreFragment -> {
-                        navigateToFragment(ExploreFragment())
-                        true
-                    }
-                    R.id.categoriesFragment -> {
-                        navigateToFragment(CategoriesFragment())
-                        true
-                    }
-                    R.id.profileFragment -> {
-                        navigateToFragment(ProfileFragment())
-                        true
-                    }
-                    else -> false
-                }
-            }
-        }
-        navController?.addOnDestinationChangedListener { _, destination, _ ->
+        AppBarConfiguration(
+            setOf(
+                R.id.exploreFragment,
+                R.id.categoriesFragment,
+                R.id.nav_profile,
+            )
+        )
+        binding.bottomNavigation.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
-                R.id.exploreFragment -> showBottomNav()
-                else -> hideBottomNav()
+                R.id.detailsBookFragment -> hideBottomNav()
+                R.id.filterFragment -> hideBottomNav()
+                else -> showBottomNav()
             }
         }
     }
@@ -70,21 +44,4 @@ class DashboardActivity: BaseActivity() {
     private fun hideBottomNav() {
         binding.bottomNavigation.visibility = View.GONE
     }
-
-    private fun navigateToFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.nav_dashboard_items, fragment)
-            .commit()
-    }
-
-    open val navController: NavController? by lazy {
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_dashboard) as NavHostFragment?
-        return@lazy navHostFragment?.navController
-    }
-
-
 }
-
-
-
